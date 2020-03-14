d2441a408a6b1254b1368d415e5d577d562fa702
Wzard <oncwnuGDYYREQp2D-dUhLySgDJOc@git.weixin.qq.com>
2020-03-14 18:04:35 +0800

    暂时

--- miniprogram/pages/reg_user/reg_user.js
+++ miniprogram/pages/reg_user/reg_user.js
@@ -5,14 +5,19 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-
+		code: [],
+		name:{required: true}
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
+		
+	},
 
+	nameRules: function (event){
+		
 	},
 
 	/**
@@ -62,5 +67,9 @@ Page({
 	 */
 	onShareAppMessage: function () {
 
+	},
+
+	_rulesCode: function () {
+		
 	}
 })
